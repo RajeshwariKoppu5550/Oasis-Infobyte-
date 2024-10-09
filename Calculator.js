590935d2c3d716df470fd1f38b212a6c061c9e75
RajeshwariKoppu <rajeshwarikoppu76@gmail.com>
2024-10-09 22:25:10 +0530

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,26 @@
+
+let displayValue = '';
+
+function appendNumber(number) {
+    displayValue += number;
+    document.getElementById('display').value = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = '';
+    document.getElementById('display').value = displayValue;
+}
+
+function deleteLast() {
+    displayValue = displayValue.slice(0, -1);
+    document.getElementById('display').value = displayValue;
+}
+
+function calculate() {
+    try {
+        displayValue = eval(displayValue);
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
